a80f47c45a862302df2b000ae946fa64ed4d4d60
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-04-11 23:03:23 +0530

    commit new UI to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div>
       <h4>Log In</h4>
-      <form>
+      <div className='flex flex-col'>
+      <form className='flex flex-col align-center'>
         <input type='email' placeholder='Email'/>
         <input type='password' placeholder='Password'/>
       </form>
-      <button>Log In</button>
+      <button className='text-white'>Log In</button>
+      </div>
+      <div className='flex'>
+      <input type='checkbox'/>
+      <p className='text-white'>Remeber Me</p>
+      <p className='text-white'>Need Help?</p>
+      </div>      
+      <p className='text-white'>New to Bit-Vishwa<Link to ='/signup'><span>Sign Up</span></Link></p>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
     <div className='flex justify-between mt-[2%]'>
         <Link to ='/'><h3 className='text-purple-500 font-Rowdies text-4xl'>Bit-Vishwa</h3></Link>
         <div  className='flex justify-around w-[20%]'>
-        <Link to ='/login'><button className='text-white hover:border-green-300 hover:px-4 hover:py-2 hover:rounded-md hover:bg-green-500'>Sign In</button></Link>
-        <Link to ='/signup'><button className='text-white  hover:border-green-300 hover:px-4 hover:py-2 hover:rounded-md hover:bg-purple-500'>Sign Up</button></Link>
+        <Link to ='/login'><button className='text-white hover:border-pink-300 hover:px-4 hover:py-2 hover:rounded-md hover:bg-pink-300'>Sign In</button></Link>
+        <Link to ='/signup'><button className='text-white  hover:border-pink-300 hover:px-4 hover:py-2 hover:rounded-md hover:bg-pink-300'>Sign Up</button></Link>
         </div>
     </div>
   )
