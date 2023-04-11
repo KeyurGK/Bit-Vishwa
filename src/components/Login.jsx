da07f1a1bab8da3ecc1037a69c989e4bdbdfbcb7
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-04-11 22:43:43 +0530

    added login page email & password input box

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <h4>Log In</h4>
+      <form>
+        <input type='email' placeholder='Email'/>
+        <input type='password' placeholder='Password'/>
+      </form>
+      <button>Log In</button>
+    </div>
   )
 }
 
