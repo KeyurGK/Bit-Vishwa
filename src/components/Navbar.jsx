f880edb2b4320f5835bbbded8d73cef5428f01d5
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-04-11 22:44:19 +0530

    deleted un necessary content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex justify:'>
-        <h3 className='text-purple-500 font-Rowdies text-4xl'>Bit-Vishwa</h3>
-        <Link to ='/login'><button className='text-white'>Sign In</button></Link>
-        <button className='text-white'>Sign Up</button>
-
+    <div className='flex justify-between mt-[2%]'>
+        <Link to ='/'><h3 className='text-purple-500 font-Rowdies text-4xl'>Bit-Vishwa</h3></Link>
+        <div  className='flex justify-around w-[20%]'>
+        <Link to ='/login'><button className='text-white hover:border-green-300 hover:px-4 hover:py-2 hover:rounded-md hover:bg-green-500'>Sign In</button></Link>
+        <Link to ='/signup'><button className='text-white  hover:border-green-300 hover:px-4 hover:py-2 hover:rounded-md hover:bg-purple-500'>Sign Up</button></Link>
+        </div>
     </div>
   )
 }
