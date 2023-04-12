d82fd61f261d7f4315629d611d63dde6759ab3e3
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-04-12 22:47:31 +0530

    axios request

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<Signup/>}/>
       </Routes>
+      <Home/>
     </div>
   );
 }
---
+++ src/Requests.js
@@ -0,0 +1,7 @@
+const key='4d100bb424mshaaaecf4bd1066f5p1b5cb0jsn7cc8632e6b51';
+const host='https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd';
+const requests={
+    requestCoin : `${host}+?api_key=${key}`
+}
+
+export default requests;
