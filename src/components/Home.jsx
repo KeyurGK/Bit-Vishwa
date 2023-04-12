d8d43978bae03d6816ee52967778c26d35d6a63f
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-04-12 21:43:01 +0530

    created Home component to dispaly crypto data

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState,useEffect } from 'react'
+
+const Home = () => {
+    const [coins,setCoins]=useState([])
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Home
