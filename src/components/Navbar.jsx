5095cdeef42456de0158e937dfbac42f57bd94cd
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-04-16 11:54:45 +0530

    improved UI of login and signup button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='flex justify-between mt-[2%]'>
-        <Link to ='/'><h3 className='text-purple-900 font-rowdies text-4xl'>Bit-Vishwa</h3></Link>
+        <Link to ='/'><img src='../images/bitVishwaLogo.png'/><h3 className='text-purple-500 font-rowdies text-4xl'>Bit-Vishwa</h3></Link>
         <div  className='flex justify-around w-[20%]'>
-        <Link to ='/login'><button className='text-white hover:border-pink-300 hover:px-4 hover:py-2 hover:rounded-md hover:bg-pink-300'>Sign In</button></Link>
-        <Link to ='/signup'><button className='text-white  hover:border-pink-300 hover:px-4 hover:py-2 hover:rounded-md hover:bg-pink-300'>Sign Up</button></Link>
+        <Link to ='/login'><button className='text-white hover:border-white px-4 py-2 hover:rounded-md hover:bg-purple-500'>Sign In</button></Link>
+        <Link to ='/signup'><button className='text-white  hover:border-white px-4 py-2 hover:rounded-md hover:bg-purple-500'>Sign Up</button></Link>
         </div>
     </div>
   )
