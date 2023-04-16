66afe30c7e1b70508bffb24453656c4b7f86f738
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-04-16 19:48:46 +0530

    final auth commit

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -21,15 +21,15 @@ const Signup = () => {
   } 
   return (
     <div className=' mx-[25%] mt-[5%] bg-gray-500 border-4 border-purple-900'>
-
+ 
       <div className='flex flex-col  justify-center pt-8'>
 
-        <form className='flex flex-col items-center justify-center mx-4' onSubmit = {handleSubmit}>
+        <form className ='flex flex-col items-center justify-center mx-4' onSubmit = {handleSubmit}>
           <input className='w-[60%] mx-[25%] px-2 py-3 border-4 rounded border-purple-900' type='email' placeholder='Email' onChange={(e)=>setEmail(e.target.value)}/>
           <input className='w-[60%] mx-[25%] mt-2 px-2 py-3 border-4 rounded border-purple-900' type='password' placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
         </form>
 
-        <button className=' w-32 h-16 mx-[40%] mt-[5%] text-black font-rowdies text-lowercase border-4 border-purple-900 bg-purple-300 hover:bg-purple-500 '>Sign Up</button>
+        <button className=' w-32 h-16 mx-[40%] mt-[5%] text-black font-rowdies text-lowercase border-4 border-purple-900 bg-purple-300 hover:bg-purple-500 ' onClick={handleSubmit}>Sign Up</button>
       </div>
 
       <div className='flex justify-around mt-4'>
