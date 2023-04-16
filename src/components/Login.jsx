1428bdd793f708a10250732134092dbca64b3dae
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-04-16 19:10:50 +0530

    commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Login = () => {
+  const [email,setEmail]=useState('')
+  const[password,setPassword]=useState('')
+  const[error,setError]=useState('')
+  const {user,logIn} = UserAuth()
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setError('')
+    try{
+      await logIn(email,password)
+      navigate('/')
+    }
+    catch(error)
+    {
+      console.log(error)
+      setError(error.message)
+    }
+  } 
   return (
     <div className=' mx-[25%] mt-[5%] bg-gray-500 border-4 border-purple-900'>
 
       <div className='flex flex-col  justify-center pt-8'>
 
-        <form className='flex flex-col items-center justify-center mx-4'>
-          <input className='w-[60%] mx-[25%] px-2 py-3 border-4 rounded border-purple-900' type='email' placeholder='Email'/>
-          <input className='w-[60%] mx-[25%] mt-2 px-2 py-3 border-4 rounded border-purple-900' type='password' placeholder='Password'/>
+        <form className='flex flex-col items-center justify-center mx-4' onSubmit={handleSubmit}>
+          <input className='w-[60%] mx-[25%] px-2 py-3 border-4 rounded border-purple-900' type='email' placeholder='Email' onChange={(e)=>setEmail(e.target.value)}/>
+          <input className='w-[60%] mx-[25%] mt-2 px-2 py-3 border-4 rounded border-purple-900' type='password' placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
         </form>
-
+        {error? <p className='p-3 bg-red-400 my-2'>{error}</p> : null}
         <button className=' w-32 h-16 mx-[40%] mt-[5%] text-black font-rowdies text-lowercase border-4 border-purple-900 bg-purple-300 hover:bg-purple-500 '>Sign In</button>
       </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,43 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Navbar = () => {
+  const {user, logOut} = UserAuth()
+  const navigate = useNavigate()
+
+  const handleLogout = async()=>{
+    try{
+      await logOut();
+      navigate('/')
+    }catch(error){
+      console.log(error)
+    }
+  }
+  console.log(user)
   return (
     <div className='flex justify-between mt-[2%]'>
         <Link to ='/'><h3 className='text-purple-500 font-rowdies text-4xl'>Bit-Vishwa</h3></Link>
-        <div  className='flex justify-around w-[20%]'>
-        <Link to ='/login'><button className='text-white hover:border-white px-4 py-2 hover:rounded-md hover:bg-purple-500'>Sign In</button></Link>
-        <Link to ='/signup'><button className='text-white  hover:border-white px-4 py-2 hover:rounded-md hover:bg-purple-500'>Sign Up</button></Link>
-        </div>
+    {user?.email?(
+    <div  className='flex justify-around w-[20%]'>
+    <Link to='/account'>
+    <button className='text-white hover:border-white px-4 py-2 hover:rounded-md hover:bg-purple-500'>Account</button>
+    </Link>
+    <Link>
+      <button onClick={handleLogout} className='text-white hover:border-white px-4 py-2 hover:rounded-md hover:bg-purple-500'>Log Out</button>
+    </Link>
+    </div>
+    ):(
+      <div  className='flex justify-around w-[20%]'>
+    <Link to ='/login'><button className='text-white hover:border-white px-4 py-2 hover:rounded-md hover:bg-purple-500'>Sign In</button></Link>
+
+    <Link to ='/signup'><button className='text-white  hover:border-white px-4 py-2 hover:rounded-md hover:bg-purple-500'>Sign Up</button></Link>
+
+    </div>
+   
+    )}
     </div>
   )
 }
-
+ 
 export default Navbar
