c57070dd2f44838267624afdd4c2de4310b79008
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-07-10 18:40:14 +0530

    deleted garbage and updated account

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import logo from './logo.svg';
-
-import './App.css';
-import Navbar from './components/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Home from './components/Home';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
 import { AuthContextProvider } from "./context/AuthContext";
+import Account from "./components/Account";
 
 function App() {
   return (
     <div className="App">
-    <AuthContextProvider>
-    <Navbar/>      
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-      </Routes>
-    </AuthContextProvider>
-      
-      
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
